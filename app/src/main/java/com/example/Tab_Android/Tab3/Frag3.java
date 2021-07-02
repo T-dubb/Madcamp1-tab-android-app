652e06a3db61ce6ea575dcccb82453bf2c3cc1bf
Toby <rlaxodntttt@kaist.ac.kr>
2021-07-02 21:37:31 +0900

    make tab3(cat background time frag)

--- app/src/main/java/com/example/Tab_Android/Tab3/Frag3.java
+++ app/src/main/java/com/example/Tab_Android/Tab3/Frag3.java
@@ -4,18 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.example.Tab_Android.R;
 
 public class Frag3 extends Fragment {
+
+    TextView dateView;
+
+    TextView cityView;
+    TextView weatherView;
+    TextView tempView;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v=inflater.inflate(R.layout.activity_frag3,container,false);
+        View v=inflater.inflate(R.layout.tab3_activity_frag3,container,false);
+        ImageButton button = v.findViewById(R.id.imageButton);
+        dateView = v.findViewById(R.id.dateView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long now = System.currentTimeMillis();
+                Date date = new Date(now);
+                SimpleDateFormat simpleDateFormatDay = new SimpleDateFormat("yyyy-MM-dd");
+                SimpleDateFormat simpleDateFormatTime = new SimpleDateFormat("HH:mm:ss");
+                String getDay = simpleDateFormatDay.format(date);
+                String getTime = simpleDateFormatTime.format(date);
+                String getDate = getDay + "\n" + getTime;
+                dateView.setText(getDate);
+            }
+        });
 
         return v;
     }
