c9a65df8b9b20ce0d060aeb8a64efe240bffa8cd
yeeunsong <yeeunsong1019@gmail.com>
2021-07-05 12:32:32 +0900

    implemented google map with location features

--- app/src/main/java/com/example/Tab_Android/Tab3/Frag3.java
+++ app/src/main/java/com/example/Tab_Android/Tab3/Frag3.java
@@ -1,9 +1,16 @@
 package com.example.Tab_Android.Tab3;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.PatternMatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,48 +24,88 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.jetbrains.annotations.NotNull;
+
+import static androidx.core.content.ContextCompat.checkSelfPermission;
 
 public class Frag3 extends Fragment {
 
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Initialize view
+        //Initialize view
         View view = inflater.inflate(R.layout.tab3_commute_map, container, false);
 
         //Initialize map fragment
-        SupportMapFragment supportMapFragment = (SupportMapFragment)
+        supportMapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.google_map);
+        //Initialize fused location
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
 
+        //Check permission
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //when permission granted
+            //call method
+            getCurrentLocation();
+        }else{
+            //When permission denied
+            //Request permission
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
 
-        //Async map
-        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                //When map is loaded
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                    @Override
-                    public void onMapClick(LatLng latLng) {
-                        //When clicked on map
-                        //Initialize marker options
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        //================Let's put a custom Icon for this marker==============
-                        markerOptions.position(latLng);
-                        //Remove all marker
-                        googleMap.clear();
-                        //Animating to zoom the marker
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                                latLng, 10
-                        ));
-                        //Add marker on map
-                        googleMap.addMarker(markerOptions);
-                    }
-                });
-            }
-        });
 
         //Return view
         return view;
     }
+
+    private void getCurrentLocation() {
+        //Initialize task location
+        @SuppressLint("MissingPermission") Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //When success
+                if(location!=null){
+                    //Sync map
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            //Initialize lat lng
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                            //Create marker options
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("here");
+                            //Zoom map
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+                            //Add marker on map
+                            googleMap.addMarker(options);
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //When permission granted
+                //Call method
+                getCurrentLocation();
+            }
+        }
+    }
+
+
 }
