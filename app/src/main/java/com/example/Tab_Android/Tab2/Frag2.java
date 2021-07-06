4905124610e7b241e48c520aaa771a86e741a90d
Toby <rlaxodntttt@kaist.ac.kr>
2021-07-06 11:23:26 +0900

    album margin

--- app/src/main/java/com/example/Tab_Android/Tab2/Frag2.java
+++ app/src/main/java/com/example/Tab_Android/Tab2/Frag2.java
@@ -13,6 +13,7 @@ import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.Tab_Android.R;
@@ -31,10 +32,59 @@ public class Frag2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab2_album, container, false);
+        View v = inflater.inflate(R.layout.tab2_album, container, false);
+        ViewGroup sceneRoot = v.findViewById(R.id.tab2_main);
+        rootScene = Scene.getSceneForLayout(sceneRoot, R.layout.tab2_album, getActivity());
+        catScene = Scene.getSceneForLayout(sceneRoot, R.layout.tab2_view_cat, getActivity());
+        foodScene = Scene.getSceneForLayout(sceneRoot, R.layout.tab2_view_food, getActivity());
+        dessertScene = Scene.getSceneForLayout(sceneRoot, R.layout.tab2_view_dessert, getActivity());
+        landScene = Scene.getSceneForLayout(sceneRoot, R.layout.tab2_view_land, getActivity());
+
+        ImageButton Album1 = (ImageButton) v.findViewById(R.id.imageView);//get the id of first image view
+        ImageView Album2 = (ImageView) v.findViewById(R.id.imageView2);//get the id of second image view
+        ImageView Album4 = (ImageView) v.findViewById(R.id.imageView4);//get the id of fourth image view
+        ImageView Album3 = (ImageView) v.findViewById(R.id.imageView3);//get the id of third image view
+
+        Transition fadeTransition = new Fade();
+
+        Album1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TransitionManager.go(catScene, fadeTransition);
+            }
+        });
+        Album2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TransitionManager.go(foodScene, fadeTransition);
+            }
+        });
+        Album3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TransitionManager.go(dessertScene, fadeTransition);
+            }
+        });
+        Album4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TransitionManager.go(landScene, fadeTransition);
+            }
+        });
+
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                TransitionManager.go(rootScene, fadeTransition);
+            }
+        };
+        getActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
+
+        return v;
 
     }
-
+/**
     public void onViewCreated(View view, Bundle SavedInstanceState) {
         ViewGroup sceneRoot = requireView().findViewById(R.id.tab2_main);
         // Create the scenes
@@ -43,14 +93,6 @@ public class Frag2 extends Fragment {
         foodScene = Scene.getSceneForLayout(sceneRoot, R.layout.tab2_view_food, getActivity());
         dessertScene = Scene.getSceneForLayout(sceneRoot, R.layout.tab2_view_dessert, getActivity());
         landScene = Scene.getSceneForLayout(sceneRoot, R.layout.tab2_view_land, getActivity());
-
-        Transition fadeTransition = new Fade();
-
-
-        ImageView Album1 = (ImageView) getView().findViewById(R.id.imageView);//get the id of first image view
-        ImageView Album2 = (ImageView) getView().findViewById(R.id.imageView2);//get the id of second image view
-        ImageView Album4 = (ImageView) getView().findViewById(R.id.imageView4);//get the id of fourth image view
-        ImageView Album3 = (ImageView) getView().findViewById(R.id.imageView3);//get the id of third image view
 
 
         Album1.setOnClickListener(new View.OnClickListener() {
@@ -85,5 +127,5 @@ public class Frag2 extends Fragment {
             }
         };
         requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
-    }
+    }*/
 }
